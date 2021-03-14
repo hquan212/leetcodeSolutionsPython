b3594ad71a613bcc0b4639ab24c8da4585992663
Henry Quan <henry.quan@liongard.com>
2021-03-14 14:16:37 -0500

    added 7

--- 7.reverse-integer.py
+++ 7.reverse-integer.py
@@ -6,7 +6,16 @@
 
 # @lc code=start
 class Solution:
+    def checkTooLarge(self, x):
+        # Check within 32 bit boundaries
+        if -2**31 <= x <= (2**31-1):
+            return False 
+        return True
+
     def reverse(self, x: int) -> int:
-        
+        if x == 0 : return 0
+        isNegative = [1,-1][x<0]  # Choose sign if x < 0
+        res = isNegative * int(str(abs(x))[::-1])  # int casting auto removes leading 0s
+        return 0 if self.checkTooLarge(res) else res
 # @lc code=end
 
