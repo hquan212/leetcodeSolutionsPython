6027337efa22c5fae84ebb54ef80ea10ff7afb43
Henry Quan <henry.quan@liongard.com>
2021-03-24 21:38:26 -0500

    sqrt functions

--- 67.add-binary.py
+++ 67.add-binary.py
@@ -7,13 +7,12 @@
 # @lc code=start
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        # THERE ARE SECRETS TO THIS LOOK BELOW! XOR them before 
-        x, y = int(a, 2), int(b, 2) # Convert from string to bin.
-        while y: 
-            answer = x^y # this is the sum without carry 
-            carry = (x&y) << 1 # carry is just and left shifted
-            x,y = answer, carry
-        return bin(x)[2:]
+        bina, binb = int(a,2), int(b,2) 
+        while (binb): 
+            xord = bina^binb 
+            carry = (bina&binb) << 1 
+            bina, binb = xord, carry 
+        return bin(bina)[2:]
 
 # @lc code=end
 
